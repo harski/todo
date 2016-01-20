069fdf131b04245c881f8a7732aeb747c9eb693a
Tuomo Hartikainen <tth@harski.org>
2016-01-20 21:43:25 +0200

    Move action parsing to fn

--- src/main.rs
+++ src/main.rs
@@ -15,29 +15,25 @@ const LICENSE_STR: &'static str =
     "Copyright 2016 Tuomo Hartikainen <tth@harski.org>.\n\
      Licensed under the 2-clause BSD license, see LICENSE for details.";
 
+enum Action {
+    Help,
+    Todo,
+    Version,
+}
+
 fn main() {
     // TODO: Parse options
-    // TODO: Parse action
     let args: Vec<String> = env::args().collect();
     let program = args[0].clone();
-
     let mut opts = Options::new();
-    opts.optflag("h", "help", "print this help");
-    opts.optflag("v", "version", "show version");
+    get_opt_strs(&mut opts);
 
-    let matches = match opts.parse(&args[1..]) {
-        Ok(m)   => { m }
-        Err(f)  => { panic!(f.to_string()) }
-    };
+    let action = get_action(&args);
 
-    if matches.opt_present("h") {
-        print_help(&program, opts);
-        return;
-    }
-
-    if matches.opt_present("v") {
-        print_version();
-        return;
+    match action {
+        Action::Help    => { print_help(&program, &opts); return; },
+        Action::Version => { print_version(); return; },
+        Action::Todo    => {},
     }
 
     let mut files: Vec<PathBuf> = Vec::new();
@@ -55,7 +51,47 @@ fn main() {
 }
 
 
-fn print_help(program: &str, opts: Options) {
+fn get_action(args: &Vec<String>) -> Action {
+    let mut opts = Options::new();
+    opts.optflag("h", "help", "print this help");
+    opts.optflag("v", "version", "show version");
+
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m)   => { m }
+        Err(f)  => { panic!(f.to_string()) }
+    };
+
+    let mut action_matches: Vec<Action> = Vec::new();
+    if matches.opt_present("h") {
+        action_matches.push(Action::Help);
+    }
+
+    if matches.opt_present("v") {
+        action_matches.push(Action::Version);
+    }
+
+    // TODO: handle with `match`?
+    if action_matches.len() > 1 {
+        // error, too many actions found
+        panic!("Error: too many actions supplied. Quitting");
+    } else if action_matches.len() == 1 {
+        match action_matches.pop() {
+            Some(ac) => ac,
+            None     => { panic!("Error reading action argument"); },
+        }
+    } else {
+        Action::Todo
+    }
+}
+
+
+fn get_opt_strs(opts: &mut Options) {
+    opts.optflag("h", "help", "print this help");
+    opts.optflag("v", "version", "show version");
+}
+
+
+fn print_help(program: &str, opts: &Options) {
     let brief = format!("Usage: {} [ACTION]", program);
     print!("{}", opts.usage(&brief));
 }
