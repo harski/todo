db3abaa6e1f3c9c777340fc1f25f8a0198b6b12c
Tuomo Hartikainen <tth@harski.org>
2016-01-28 21:29:17 +0200

    Move actions from main to action.rs

--- src/action.rs
+++ src/action.rs
@@ -1,9 +1,77 @@
 // Copyright 2016 Tuomo Hartikainen <tth@harski.org>.
 // Licensed under the 2-clause BSD license, see LICENSE for details.
 
+extern crate time;
+
+use std::rc::Rc;
+
+use getopts::Options;
+
+use todo_item::TodoItem;
+use todo_items;
+
 pub enum Action {
     Dump,
     Help,
     Today,
     Version,
+}
+
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+const LICENSE_STR: &'static str =
+    "Copyright 2016 Tuomo Hartikainen <tth@harski.org>.\n\
+     Licensed under the 2-clause BSD license, see LICENSE for details.";
+
+
+pub fn dump(items: &Vec<Rc<TodoItem>>) {
+    for item in items {
+        println!("{:?}", item);
+    }
+}
+
+
+fn get_date_today() -> Result<String, time::ParseError> {
+    let now = time::now();
+    match time::strftime("%Y-%m-%d", &now) {
+        Ok(d_str)   => Ok(d_str),
+        Err(err)    => Err(err),
+    }
+}
+
+
+pub fn print_help(program: &str, opts: &Options) {
+    let brief = format!("Usage: {} [ACTION]", program);
+    print!("{}", opts.usage(&brief));
+}
+
+
+pub fn print_today(items: &Vec<Rc<TodoItem>>) {
+    let today_str = match get_date_today() {
+        Ok(date)    => date,
+        Err(err)    => {
+            print_err!("Could not get today's date: {}", err);
+            return;
+        },
+    };
+
+    let todays = todo_items::filter_items_on_date(&items, &today_str);
+
+    let mut first = true;
+    for item in todays {
+        match first {
+            true    => first = false,
+            false   => println!(""),
+        };
+
+        println!("{}", item.heading);
+        for attr in &item.attrs {
+            println!("{}: {}", attr.key, attr.value);
+        }
+    }
+}
+
+
+pub fn print_version() {
+    println!("todo version {}", VERSION);
+    println!("{}", LICENSE_STR);
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@
 // Licensed under the 2-clause BSD license, see LICENSE for details.
 
 extern crate getopts;
-extern crate time;
 
 // keep macros up here to be able to use them in submodules
 #[macro_use]
@@ -16,17 +15,10 @@ mod todo_items;
 
 use getopts::Options;
 use std::env;
-use std::rc::Rc;
 
 use action::Action;
 use opt::Opt;
-use todo_item::TodoItem;
-use todo_items::{filter_items_on_date, get_todo_items};
-
-const VERSION: &'static str = env!("CARGO_PKG_VERSION");
-const LICENSE_STR: &'static str =
-    "Copyright 2016 Tuomo Hartikainen <tth@harski.org>.\n\
-     Licensed under the 2-clause BSD license, see LICENSE for details.";
+use todo_items::get_todo_items;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -42,8 +34,8 @@ fn main() {
 
     // "trivial" actions, always return
     match action {
-        Action::Help    => { print_help(&program, &opts_in); return; },
-        Action::Version => { print_version(); return },
+        Action::Help    => { action::print_help(&program, &opts_in); return; },
+        Action::Version => { action::print_version(); return },
         _               => { },
     }
 
@@ -51,65 +43,11 @@ fn main() {
     match get_todo_items(opts.todo_dir.as_path()) {
         Ok(items)   => {
             match action {
-                Action::Dump    => { dump(&items); },
-                Action::Today   => { print_today(&items); },
+                Action::Dump    => { action::dump(&items); },
+                Action::Today   => { action::print_today(&items); },
                 _               => {},
             }
         },
         Err(err)    => print_err!("Could not parse todo items: {}", err),
     };
-}
-
-
-fn dump(items: &Vec<Rc<TodoItem>>) {
-    for item in items {
-        println!("{:?}", item);
-    }
-}
-
-
-fn get_date_today() -> Result<String, time::ParseError> {
-    let now = time::now();
-    match time::strftime("%Y-%m-%d", &now) {
-        Ok(d_str)   => Ok(d_str),
-        Err(err)    => Err(err),
-    }
-}
-
-
-fn print_help(program: &str, opts: &Options) {
-    let brief = format!("Usage: {} [ACTION]", program);
-    print!("{}", opts.usage(&brief));
-}
-
-
-fn print_today(items: &Vec<Rc<TodoItem>>) {
-    let today_str = match get_date_today() {
-        Ok(date)    => date,
-        Err(err)    => {
-            print_err!("Could not get today's date: {}", err);
-            return;
-        },
-    };
-
-    let todays = filter_items_on_date(&items, &today_str);
-
-    let mut first = true;
-    for item in todays {
-        match first {
-            true    => first = false,
-            false   => println!(""),
-        };
-
-        println!("{}", item.heading);
-        for attr in &item.attrs {
-            println!("{}: {}", attr.key, attr.value);
-        }
-    }
-}
-
-
-fn print_version() {
-    println!("todo version {}", VERSION);
-    println!("{}", LICENSE_STR);
 }
